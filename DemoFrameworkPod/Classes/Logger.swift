61e8609319b0d16e112755593f16208877b473b2
Tung Vu Duc <tungvuduc2805@gmail.com>
2021-02-03 22:46:20 +0700

    first commit

---
+++ DemoFrameworkPod/Classes/Logger.swift
@@ -0,0 +1,16 @@
+//
+//  Logger.swift
+//  DemoFrameworkPod
+//
+//  Created by Tung Vu Duc on 03/02/2021.
+//
+
+import Foundation
+
+public class Logger {
+    public init() {}
+    
+    public func printLog() {
+        print("Hello World")
+    }
+}
